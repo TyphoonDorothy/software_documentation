e6a8c9bd47792617776d78e78dd42ddd48a49f22
TyphoonDorothy <odarkaz66@gmail.com>
2026-05-03 15:51:03 +0300

    added lab 4

--- lab2/app.py
+++ lab2/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from data_access.repositories import SqlAlchemyRepository
 from business.services import ListingService
 
